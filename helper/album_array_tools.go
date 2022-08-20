a8e206f92b14dab1165d1530cad11b17cee55bce
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-08-21 00:08:44 +0530

    commit added

--- helper/album_array_tools.go
+++ helper/album_array_tools.go
@@ -3,7 +3,6 @@ package album_array_tools
 import (
 	"errors"
 	. "web-server/model"
-
 )
 
 func RemoveAlbumAt(array []AlbumModel, index int) ([]AlbumModel ,*AlbumModel, error) {
@@ -12,10 +11,13 @@ func RemoveAlbumAt(array []AlbumModel, index int) ([]AlbumModel ,*AlbumModel, er
 		if arrayIndex == index {
 			for i := arrayIndex; i < (len(array) - 1); i++ {
 				array[i] = array[i+1]
+				//If any index matched, then just insert/append the newly assigned element till the for loop end.
 				newArray = append(newArray, array[i])
 			}
+			//then return to terminate the function
 			return newArray,&album,nil
 		} else {
+			//insert/append the album from the old array to the new array until if condition satisfied.
 			newArray = append(newArray, album)
 		}
 	}
