ae091bd0047616542695a8a246f724fb1e57aa17
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-09-04 17:17:01 +0530

    id bson added

--- model/album_model.go
+++ model/album_model.go
@@ -1,15 +1,14 @@
 package album_model
 
-type AddAlbumModel struct{
-	Title string `form:"title" binding:"required"`
-	Artist string `form:"artist" binding:"required"`
-	Price float64 `form:"price" binding:"required"`
+type AddAlbumModel struct {
+	Title  string  `form:"title" binding:"required" bson:"title"`
+	Artist string  `form:"artist" binding:"required" bson:"artist"`
+	Price  float64 `form:"price" binding:"required" bson:"price"`
 }
 
-
-type AlbumModel struct{
-	Id string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
+type AlbumModel struct {
+	Id     string  `json:"id" bson:"_id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
