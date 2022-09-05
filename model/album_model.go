577004490959ee59c32028914132f1dbffbc3d43
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-09-06 00:08:06 +0530

    multipart file header added with image

--- model/album_model.go
+++ model/album_model.go
@@ -1,14 +1,22 @@
 package album_model
 
+import "mime/multipart"
+
 type AddAlbumModel struct {
-	Title  string  `form:"title" binding:"required" bson:"title"`
-	Artist string  `form:"artist" binding:"required" bson:"artist"`
-	Price  float64 `form:"price" binding:"required" bson:"price"`
+	Title  string  `form:"title" binding:"required"`
+	Artist string  `form:"artist" binding:"required"`
+	Price  float64 `form:"price" binding:"required"`
+	Image  *multipart.FileHeader `form:"image" binding:"required"`
 }
 
 type AlbumModel struct {
 	Id     string  `json:"id" bson:"_id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Title  string  `json:"title" bson:"title"`
+	Artist string  `json:"artist" bson:"artist"`
+	Price  float64 `json:"price" bson:"price"`
+	Image  string 	`json:"image" bson:"image"`
+}
+
+type MongoImage struct {
+    Id string `bson:"_id"`
 }
